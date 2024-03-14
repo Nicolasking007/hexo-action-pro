91466a844b3ed4af6f1f2696844728284a7b33b5
Nicolas-kings <nicolas-kings@qq.com>
2024-03-14 14:28:23 +0800

    适配Butterfly主题

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM node:12-buster-slim
+FROM node:18.15.0-buster-slim
 
-LABEL version="1.0.3"
-LABEL repository="https://github.com/sma11black/hexo-action"
-LABEL homepage="https://sma11black.github.io"
-LABEL maintainer="sma11black <smallblack@outlook.com>"
+LABEL version="1.0.0"
+LABEL repository="https://github.com/Nicolasking007/hexo-action-pro"
+LABEL homepage="https://Nicolasking007.github.io"
+LABEL maintainer="Nicolasking007 <nicolas-king@qq.com>"
 
 COPY entrypoint.sh /entrypoint.sh
 COPY sync_deploy_history.js /sync_deploy_history.js
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,26 +8,36 @@ echo "$INPUT_DEPLOY_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 
+
+
 # setup deploy git account
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
 
 # install hexo env
 npm install hexo-cli -g
-npm install hexo-deployer-git --save
+npm install
+
 
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
 then
-    hexo g -d
+    # 避免hexo-butterfly-douban构建冲突 使用 hexo g -deploy
+    hexo clean
+    hexo g
+    hexo deploy
+
 elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
 then
     # pull original publish repo
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d
+    hexo clean
+    hexo g
+    hexo deploy
 else
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d -m "$INPUT_COMMIT_MSG"
+    hexo clean
+    hexo g -deploy -m "$INPUT_COMMIT_MSG"
 fi
 
 echo ::set-output name=notify::"Deploy complate."
